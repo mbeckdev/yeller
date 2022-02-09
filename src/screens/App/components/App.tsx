a69efaf79a319346d30ac992cf039d0fe931cc82
mbeckdev <mbeckdev42@gmail.com>
2022-02-09 10:47:12 -0700

    Initial routes created in app.tsx

--- src/screens/App/components/App.tsx
+++ src/screens/App/components/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import './App.css';
 import routeConstants from 'shared/constants/routes';
+import birdBlue from 'assets/yellerBirdBlueMinified.svg';
 
 const { LOGIN, HOME, NOTIFICATIONS, PROFILE } = routeConstants;
 
@@ -14,38 +15,31 @@ function App() {
   return (
     <Router>
       <div className="App">
-        {/* <div className="App">
+        <div className="App">
           <h1>App Component</h1>
+          <img src={birdBlue} height="100" width="100" alt="bird" />
           <ul className="App-nav-list">
             <li className="App-nav-item">
               <Link to={LOGIN.route}>{LOGIN.name}</Link>
             </li>
             <li className="App-nav-item">
-              <Link to={DASHBOARD.route}>{DASHBOARD.name}</Link>
+              <Link to={HOME.route}>{HOME.name}</Link>
             </li>
             <li className="App-nav-item">
-              <Link to={LISTING.route}>{LISTING.name}</Link>
+              <Link to={NOTIFICATIONS.route}>{NOTIFICATIONS.name}</Link>
             </li>
             <li className="App-nav-item">
               <Link to={PROFILE.route}>{PROFILE.name}</Link>
             </li>
           </ul>
           <Routes>
-            <Route exact path={LOGIN.route}>
-              <h1>{LOGIN.name}</h1>
-            </Route>
-            <Route path={DASHBOARD.route}>
-              <h1>{DASHBOARD.name}</h1>
-            </Route>
-            <Route path={LISTING.route}>
-              <h1>{LISTING.name}</h1>
-            </Route>
-            <Route path={PROFILE.route}>
-              <h1>{PROFILE.name}</h1>
-            </Route>
+            <Route path={'/'} element={LOGIN.name} />
+            <Route path={LOGIN.route} element={LOGIN.name} />
+            <Route path={HOME.route} element={HOME.name} />
+            <Route path={NOTIFICATIONS.route} element={NOTIFICATIONS.name} />
+            <Route path={PROFILE.route} element={PROFILE.name} />
           </Routes>
-        </div> */}
-        asdf
+        </div>
       </div>
     </Router>
   );
@@ -55,7 +49,7 @@ export default App;
 
 // {/* <header className="App-header">
 //           {/* <img src={birdGrey} height="100" width="100" alt="bird" />
-//         <img src={birdBlue} height="100" width="100" alt="bird" />
+// <img src={birdBlue} height="100" width="100" alt="bird" />
 //         <img src={birdWhite} height="100" width="100" alt="bird" />
 //         <img src={birdBlueTest} height="100" width="100" alt="bird" /> */}
 
