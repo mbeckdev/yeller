a405b2cb019535190d314ca442668af7b09e367d
mbeckdev <mbeckdev42@gmail.com>
2022-02-08 17:47:27 -0700

    Add app and shared folders

--- src/screens/App/components/App.tsx
+++ src/screens/App/components/App.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 
-import birdGrey from './assets/yellerBirdGreyMinified.svg';
-import birdWhite from './assets/yellerBirdWhiteMinified.svg';
-import birdBlue from './assets/yellerBirdBlueMinified.svg';
-import birdBlueTest from './assets/yellerBirdBlueTest.svg';
+// import birdGrey from '../../assets/yellerBirdGreyMinified.svg';
+// import birdWhite from '../../assets/yellerBirdWhiteMinified.svg';
+// import birdBlue from '../../assets/yellerBirdBlueMinified.svg';
+// import birdBlueTest from '../../assets/yellerBirdBlueTest.svg';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={birdGrey} height="100" width="100" alt="bird" />
+        {/* <img src={birdGrey} height="100" width="100" alt="bird" />
         <img src={birdBlue} height="100" width="100" alt="bird" />
         <img src={birdWhite} height="100" width="100" alt="bird" />
-        <img src={birdBlueTest} height="100" width="100" alt="bird" />
+        <img src={birdBlueTest} height="100" width="100" alt="bird" /> */}
 
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
@@ -26,6 +26,7 @@ function App() {
         >
           Learn React
         </a>
+        <div>LOOOOOOOOOOOOOOOOOL</div>
       </header>
     </div>
   );
---
+++ src/shared/routes.tsx
@@ -0,0 +1,18 @@
+export default Object.freeze({
+  LOGIN: {
+    name: 'Login',
+    route: '/login',
+  },
+  HOME: {
+    name: 'Home',
+    route: '/home',
+  },
+  NOTIFICATIONS: {
+    name: 'Notifications',
+    route: '/notifications',
+  },
+  PROFILE: {
+    name: 'Profile',
+    route: '/me',
+  },
+});
